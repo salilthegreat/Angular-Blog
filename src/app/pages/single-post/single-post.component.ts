a3c89ab1257971d7dd8870c4791afc0e114a75eb
salilthegreat <salilkumardas22@gmail.com>
2024-01-17 18:03:35 +0530

    Single Post Component design Complete

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { CommentFormComponent } from '../../comments/comment-form/comment-form.component';
+import { CommentsListComponent } from '../../comments/comments-list/comments-list.component';
 
 @Component({
   selector: 'app-single-post',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent, CommentFormComponent, CommentsListComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })

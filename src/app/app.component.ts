aec0dd647680ad1d5a50202d7ac8e7813adf100b
salilthegreat <salilkumardas22@gmail.com>
2024-01-17 14:32:54 +0530

    Homepage Design Complete:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
 import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,CategoryNavbarComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,CategoryNavbarComponent,FooterComponent,PostCardComponent,SubscriptionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

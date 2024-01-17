eb09bfe1bee8e58c730ce130e3b589712e8e5adb
salilthegreat <salilkumardas22@gmail.com>
2024-01-17 14:47:17 +0530

    Single Category Page Design Complete

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 
 @Component({
   selector: 'app-single-category',
   standalone: true,
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './single-category.component.html',
   styleUrl: './single-category.component.css'
 })
